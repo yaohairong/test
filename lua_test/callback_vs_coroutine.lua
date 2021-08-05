6772c27cbd45e2f45142e945d2eeaead115d26e0
Yao Hairong <yaohairong314@163.com>
2021-08-05 10:52:50 +0800

    Update callback_vs_coroutine.lua

--- lua_test/callback_vs_coroutine.lua
+++ lua_test/callback_vs_coroutine.lua
@@ -13,12 +13,13 @@ local function coroutinize(func, ...)
     exec(...)
 end
 
-local function req_a(text1, text2, callback)
+local M = {}
+function M.req_a(text1, text2, callback)
     print("running req_a, args:",  text1, text2)
     callback(text1 .. text2)
 end
 
-local function req_b(text, callback)
+function M.req_b(text, callback)
     print("running req_b, args:",  text)
     callback(text)
 end
@@ -32,31 +33,30 @@ local function co(co_callback, ...)
 end
 
 print("callback hell:")
-req_a("a1", "a2", function(data1)
+M.req_a("a1", "a2", function(data1)
     print("req_a return:",  data1)
-    req_b("b1", function(data2)
+    M.req_b("b1", function(data2)
         print("req_b return:",  data2)
-        req_a("aa1", "aa2", function(data3)
+        M.req_a("aa1", "aa2", function(data3)
             print("req_a return:", data3)
         end)
     end)
 end)
 
-local old_req_a = req_a
-req_a = function (text1, text2)
-    return co(old_req_a, text1, text2)
+local function co_m(m, func_name)
+    m[func_name .. "_async"] = function(...)
+        return co(m[func_name], ...)
+    end
 end
-local old_req_b = req_b
-req_b = function(text)
-    return co(old_req_b, text)
-end
+co_m(M, "req_a")
+co_m(M, "req_b")
 
 print("\ncoroutine:")
 coroutinize(function(text1, text2)
-    local data1 = req_a(text1, text2)
+    local data1 = M.req_a_async(text1, text2)
     print("req_a return:",  data1)
-    local data2 = req_b("b1")
+    local data2 = M.req_b_async("b1")
     print("req_b return:",  data2)
-    local data3 = req_a("aa1", "aa2")
+    local data3 = M.req_a_async("aa1", "aa2")
     print("req_a return:",  data3)
 end, "a1", "a2")
