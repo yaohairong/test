d00c31727c3fcaf9f5c3beb2af292e87860f7a29
Yao Hairong <yaohairong314@163.com>
2022-01-29 18:29:27 +0800

    Update dotnet_publish.sh

--- dotnet_publish.sh
+++ dotnet_publish.sh
@@ -1,1 +1,1 @@
-dotnet publish ConsoleApp1 -c Release -r ubuntu.20.04-x64 -p:PublishReadyToRun=true --self-contained=false /p:PublishSingleFile=true
+dotnet publish ConsoleApp1 -c Release -r ubuntu.20.04-x64 -p:PublishReadyToRun=true --self-contained=false /p:PublishSingleFile=true /p:EnableCompressionInSingleFile=true
